bab29e3bb1d75904fbabae46a4c390ab3a7e5572
PodySIN <daniil2007xboctob@gmail.com>
2026-05-21 22:20:37 +0300

    Add IOguard

--- hvostov.daniil/T2/types.hpp
+++ hvostov.daniil/T2/types.hpp
@@ -1,4 +1,6 @@
+#include <ios>
 #include <iostream>
+#include <istream>
 
 namespace hvostov {
 
@@ -7,4 +9,18 @@ namespace hvostov {
     unsigned long long key2;
     std::string key3;
   };
+
+  class IOguard {
+  public:
+    explicit IOguard(std::basic_ios< char >& s);
+    ~IOguard();
+
+  private:
+    std::basic_ios< char >& s_;
+    std::streamsize width_;
+    std::streamsize precision_;
+    std::basic_ios< char >::fmtflags fmt_;
+    char fill_;
+  };
+
 }
