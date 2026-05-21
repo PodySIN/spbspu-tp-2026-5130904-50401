b85aab7efee89d719ef344c84d9eb8997d6073f3
PodySIN <daniil2007xboctob@gmail.com>
2026-05-21 22:22:52 +0300

    Add io for DataStruct but without other types implementation

--- hvostov.daniil/T2/types.cpp
+++ hvostov.daniil/T2/types.cpp
@@ -1,4 +1,38 @@
 #include "types.hpp"
+#include <cmath>
+
+std::istream& hvostov::operator>>(std::istream& in, DataStruct& dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry) {
+    return in;
+  }
+  DataStruct input;
+  using sep = Delimiter;
+  using key = KeyI;
+  in >> sep{'('};
+  in >> sep{':'} >> key{input};
+  in >> sep{':'} >> key{input};
+  in >> sep{':'} >> key{input};
+  in >> sep{':'} >> sep{')'};
+  if (in) {
+    dest = input;
+  }
+  return in;
+}
+
+std::ostream& hvostov::operator<<(std::ostream& out, const DataStruct& data)
+{
+  std::ostream::sentry sentry(out);
+  if (!sentry) {
+    return out;
+  }
+  IOguard fmtguard(out);
+  out << "(:key1 " << DoubleSciO{data.key1};
+  out << ":key2 " << UllOctO{data.key2};
+  out << ":key3 \"" << data.key3 << "\":)";
+  return out;
+}
 
 hvostov::IOguard::IOguard(std::basic_ios< char >& s) :
   s_(s),
