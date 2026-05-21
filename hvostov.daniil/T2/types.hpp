d4b37a131c20ae42e40077b9999bcfb80b85c07d
PodySIN <daniil2007xboctob@gmail.com>
2026-05-21 22:21:12 +0300

    Add other types for input/output

--- hvostov.daniil/T2/types.hpp
+++ hvostov.daniil/T2/types.hpp
@@ -9,6 +9,44 @@ namespace hvostov {
     unsigned long long key2;
     std::string key3;
   };
+  std::istream& operator>>(std::istream& in, DataStruct& dest);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& dest);
+  bool operator<(const DataStruct& lhs, const DataStruct& rhs);
+
+  struct Delimiter {
+    char exp;
+  };
+  std::istream& operator>>(std::istream& in, Delimiter&& dest);
+
+  struct UllOctI {
+    unsigned long long& ref;
+  };
+  std::istream& operator>>(std::istream& in, UllOctI&& dest);
+
+  struct UllOctO {
+    const unsigned long long& ref;
+  };
+  std::ostream& operator<<(std::ostream& out, const UllOctO& dest);
+
+  struct DoubleSciI {
+    double& ref;
+  };
+  std::istream& operator>>(std::istream& in, DoubleSciI&& dest);
+
+  struct DoubleSciO {
+    const double& ref;
+  };
+  std::ostream& operator<<(std::ostream& out, const DoubleSciO& dest);
+
+  struct StringIO {
+    std::string& ref;
+  };
+  std::istream& operator>>(std::istream& in, StringIO&& dest);
+
+  struct KeyI {
+    DataStruct& ref;
+  };
+  std::istream& operator>>(std::istream& in, KeyI&& key);
 
   class IOguard {
   public:
